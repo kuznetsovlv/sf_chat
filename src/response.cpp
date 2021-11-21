3e980d5ea26f9fd1a53b40115a213582356c7c33
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2021-11-21 21:14:29 +0300

    Finished Response class

--- src/response.cpp
+++ src/response.cpp
@@ -1,2 +1,63 @@
 #include <string>
 #include "response.h"
+
+template<class T>
+Response<T>::Response(bool success, std::string messge)noexcept:Response(success, message, nullptr)
+{
+}
+
+template<class T>
+Response<T>::Response(bool success, std::string message, T *data)noexcept:_success(success),_message(message),_data(data)
+{
+}
+
+template<class T>
+Response<T>::Response(Response<T> &that)noexcept:_success(that._success),_message(that._message),_data(that._data)
+{
+}
+
+template<class T>
+Response<T>::Response(Response<T> &&that)noexcept:_success(that._success),_message(that._message),_data(that._data)
+{
+	that._data = nullptr;
+}
+
+template<class T>
+bool Response<T>::success()const noexcept
+{
+	return _success;
+}
+
+template<class T>
+std::string Response<T>::message()const noexcept
+{
+	return _message;
+}
+
+template<class T>
+T *Response<T>::data()const noexcept
+{
+	return _data;
+}
+
+template<class T>
+Response<T> &Response<T>::operator=(Response<T> &that)noexcept
+{
+	_success = that._succese;
+	_message = that._message;
+	_data = that._data;
+
+	return *this;
+}
+
+template<class T>
+Response<T> &&Response<T>::operator=(Response<T> &&that)noexcept
+{
+	_success = that._succese;
+	_message = that._message;
+	_data = that._data;
+
+	that._data = nullptr;
+
+	return *this;
+}
