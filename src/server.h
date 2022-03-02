1e55fb7ec9e9e4998ecad341429bba6b74b4a98c
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2022-03-03 00:49:35 +0300

    Rediscribed Server class

--- src/server.h
+++ src/server.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <memory>
 #include <string>
-#include <unordered_map>
 #include <set>
 #include <vector>
 #include "dataResponse.h"
@@ -12,36 +11,31 @@
 #include "user.h"
 #include "users.h"
 
-class Client;
-class Server final:public std::enable_shared_from_this<Server>
+class Server final
 {
 	private:
+	int _sockd;
 	Users _users;
-	// Indexes of last messgae sent to each client
-	std::unordered_map<std::shared_ptr<Client>, size_t> _send_from;
 	// Messages
 	Messages _messages;
 	// Connected clients
-	std::set<std::shared_ptr<Client>> _clients;
+	std::set<uint32_t> _clients;
 
 	bool hasUser(const std::string&);
 	void createUser(const std::string&, const std::string&, const std::string&);
 	void saveMessage(const Message&);
-	void subscribe(std::shared_ptr<Client>);
-	void unsubscribe(std::shared_ptr<Client>);
-	bool subscribed(std::shared_ptr<Client> )const noexcept;
-	const std::shared_ptr<Message> message(const std::shared_ptr<Client>);
+	void subscribe(uint32_t);
+	void unsubscribe(uint32_t);
+	bool subscribed(uint32_t )const noexcept;
+	const std::shared_ptr<Message> message(const uint32_t);
 
 	public:
 	Server();
 	Server(Server&) = delete;
 	Server(Server&&) = delete;
+	~Server();
 
-	Response request(RegistrationRequest&)noexcept;
-	DataResponse<std::shared_ptr<User>> request(LoginRequest&)noexcept;
-	Response request(LogoutRequest&)noexcept;
-	Response request(SendMessageRequest&)noexcept;
-	DataResponse<Message> request(GetMessageRequest&)noexcept;
+	void start();
 
 	Server &operator=(Server&) = delete;
 	Server &&operator=(Server&&) = delete;
